73503505d2dbc931c2d7205f65a8eabe00f8689f
nazayd598 <aydemirnaz1938@gmail.com>
2025-12-25 13:23:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,12 @@
 import express from "express";
-import fetch from "node-fetch";
+import factRoute from "./routes/factRoute.js";
+import adviceRoute from "./routes/adviceRoute.js";
 
 const app = express();
 const PORT = 3000;
 
-/* FACT API */
-app.get("/fact", async (req, res) => {
-  const r = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/random");
-  const d = await r.json();
-
-  res.json({
-    fact: d.text
-  });
-});
-
-/* ADVICE API (fact API'yi kullanır) */
-app.get("/advice", async (req, res) => {
-  const factRes = await fetch("http://localhost:3000/fact");
-  const fact = await factRes.json();
-
-  const adviceRes = await fetch("https://api.adviceslip.com/advice");
-  const advice = await adviceRes.json();
-
-  res.json({
-    based_on_fact: fact.fact,
-    advice: advice.slip.advice
-  });
-});
+app.use("/fact", factRoute);
+app.use("/advice", adviceRoute);
 
 app.listen(PORT, () => {
   console.log("API running on port " + PORT);
